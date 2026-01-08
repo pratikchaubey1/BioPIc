9690f153fbd7d5c238d23dcd3399fe6cdc8048ad
prab <pratikup98@gmail.com>
2026-01-08 19:24:14 +0530

    transfer

---
+++ src/Component/HandingPage/Herosection.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import BackGround from "../../assets/BackGround.webp";
+
+function Herosection() {
+  return (
+    <section className="relative min-h-screen w-full overflow-hidden">
+
+      {/* IMAGE */}
+      <img
+        src={BackGround}
+        alt="Hero"
+        className="
+          absolute inset-0
+          w-full h-full
+          object-cover
+          object-[50%_30%]
+          grayscale
+          z-0
+        "
+      />
+
+      {/* DARK OVERLAY */}
+      
+
+      {/* VIGNETTE */}
+      
+
+      {/* FILM GRAIN */}
+      <div className="absolute inset-0 noise opacity-20 pointer-events-none z-30" />
+
+      {/* EMPTY CONTENT SPACE (HOTC STYLE) */}
+      <div className="relative z-40 min-h-screen" />
+
+    </section>
+  );
+}
+
+export default Herosection;
